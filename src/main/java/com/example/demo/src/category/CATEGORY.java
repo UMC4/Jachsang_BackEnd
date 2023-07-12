4cd71a12c54896bf64c0a5e1e2c4582d0fd469d5
puradak <bbln2252@naver.com>
2023-07-13 00:25:48 +0900

    카테고리 enum 수정 - CATEGORY로 통합 및 메서드 생성

---
+++ src/main/java/com/example/demo/src/category/CATEGORY.java
@@ -0,0 +1,35 @@
+package com.example.demo.src.category;
+
+public enum CATEGORY {
+    //커뮤니티(1) : 맛집이야기(1), 질문있어요(2), 대화해요(3), 공유해요(4)
+    COMMUNITY(10,"커뮤니티"),
+    HOT_PLACE(11,"맛집이야기"),
+    QUESTION(12,"질문있어요"),
+    TALKING(13,"대화해요"),
+    SHARING(14,"공유해요"),
+    //공동구매(2) : 식재료(1), 생활용품(2), 기타(3)
+    GROUP_PURCHASE(20,"공동구매"),
+    FOODS(21,"식재료"),
+    HOUSEHOLDS(22,"생활용품"),
+    ETC(23,"기타"),
+    //레시피(3)
+    RECIPE(30,"레시피");
+
+    private final int number;
+    private final String name;
+
+    CATEGORY(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public static int getNumber(String name) {
+        for(CATEGORY c : CATEGORY.values()) {
+            if(c.getName().equals(name)) return c.number;
+        }
+        return 0;
+    }
+}
